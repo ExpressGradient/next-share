310a78ecca5052bed9f90cf95656be0430dbaf61
Praneeth <johndoeknown@protonmail.com>
2020-12-09 02:07:48 +0530

    added firebase cloud storage

--- pages/_components/UploadButton.tsx
+++ pages/_components/UploadButton.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface UploadButtonProps {
-    onClick: () => void
+    fileHandler: (object) => void
 }
 
-const UploadButton: React.FC<UploadButtonProps> = (props) => <div className={"text-center w-screen"}>
-    <button onClick={props.onClick} className={"text-lg md:text-xl bg-gradient-to-r from-white to-gray-300 px-8 py-2 rounded shadow-xl active:shadow-none"}>{props.children}</button>
-</div>
+const UploadButton: React.FC<UploadButtonProps> = (props) => {
+    const inputRef = useRef(null);
+
+    const onChangeHandler = () => {
+        props.fileHandler(inputRef.current.files);
+    }
+
+    const onClickHandler = () => {
+        inputRef.current.click();
+    }
+
+    return <div className={"text-center w-screen"}>
+        <button className={"text-lg md:text-xl bg-gradient-to-r from-white to-gray-300 px-8 py-2 rounded shadow-xl active:shadow-none"} ref={inputRef} onClick={onClickHandler}>{props.children}</button>
+        <input type="file" ref={inputRef} className={"hidden"} onChange={onChangeHandler} multiple={true}/>
+    </div>
+}
 
 export default UploadButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./_components/Header";
 import Head from "next/head";
 import UploadButton from "./_components/UploadButton";
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { getClientIp } from "request-ip";
+import {getFiles, uploadFile} from "./_firebase_config";
 
 const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (props) => {
+    const fileHandler = (files) => {
+        for(let i=0; i < files.length; i++) {
+            uploadFile(props.clientIp, files[i], () => window.alert(`${files[i].name} uploaded!`))
+        }
+    }
+
+    useEffect(() => {
+        getFiles(props.clientIp).then(response => response.items.forEach(item => console.log(item._delegate._location.path.split("/")[1])));
+    }, []);
+
     return <main className={"bg-gradient-to-r from-purple-700 to-blue-300 bg-cover h-screen"}>
         <Head>
             <title>Next-Share</title>
             <meta name={"description"} content={"Cloud-Local Network File Sharing"}/>
             <meta name={"author"} content={"Express Gradient"}/>
         </Head>
+
         <Header title={"Next-Share"} subTitle={"Cloud-Local Network File Sharing"}/>
         <h1 className={"text-center text-black text-2xl md:text-3xl my-4"}>Room <br/>{props.clientIp}</h1>
-        <UploadButton onClick={() => console.log("File Uploaded...")}>Upload File</UploadButton>
+        <UploadButton fileHandler={fileHandler}>Upload Files</UploadButton>
     </main>
 }
 
