9ce9d66310816727a73f825ba6a925e1e9269e11
Praneeth <johndoeknown@protonmail.com>
2020-12-08 22:14:04 +0530

    fixed client ip issue

--- pages/api/ip_handler.ts
+++ pages/api/ip_handler.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getClientIp } from "request-ip";
 
 export default (request: NextApiRequest, response: NextApiResponse) => {
-    const client_ip: string = request.connection.remoteAddress;
+    const client_ip: string = getClientIp(request);
     console.log(`${client_ip} connected`);
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
